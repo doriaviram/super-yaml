c15b600aab902bce1f8647e3b92b96a026aaf5e4
Dori Aviram <dori@lumigo.io>
2021-04-15 22:11:27 +0300

    chore: playgound version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export { run } from "@oclif/command";
+import {dump, load} from "js-yaml";
+import {SyntaxParserService} from "./services/syntax-parser.service";
+import {SymlSyntax} from "./types/syntax";
+import {CompileService} from "./services/compile.service";
+
+export const compile = (yml: string) => {
+  const ymlObj = load(yml);
+  const parsed = SyntaxParserService.parseSyntax(ymlObj as SymlSyntax)
+  const result = CompileService.compileSaml(parsed.clientData,parsed.types);
+  return dump(result);
+}
