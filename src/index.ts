969369bff4dcb409e8f208aafa0a62393491c212
Dori Aviram <dori@lumigo.io>
2021-04-15 15:59:21 +0300

    fix: expose compile

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import {SyntaxParserService} from "./services/syntax-parser.service";
+import {SymlSyntax} from "./types/syntax";
+import {CompileService} from "./services/compile.service";
+import {load, dump} from "js-yaml";
+
+//TODO: Move from index
+export const compile = (yml: string) => {
+  const ymlObj = load(yml);
+  const parsed = SyntaxParserService.parseSyntax(ymlObj as SymlSyntax)
+  const result = CompileService.compileSaml(parsed.clientData,parsed.types);
+  return dump(result);
+}
+
 export { run } from "@oclif/command";
