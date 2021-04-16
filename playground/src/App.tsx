1ba8396073d66c9b4772b7f217ca270ffbb170e6
Dori Aviram <dori@lumigo.io>
2021-04-16 18:09:25 +0300

    chore(playground): deploy playground

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,25 +1,27 @@
 import React, {useEffect, useState} from "react";
-import logo from "./logo.svg";
 import "./App.scss";
 import AceEditor from "react-ace";
 import {ImArrowRight} from "react-icons/all";
 import {compile} from "super-yaml";
 
 import "ace-builds/src-noconflict/mode-yaml";
-import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/mode-makefile";
+import "ace-builds/src-noconflict/theme-chrome";
 
 const DEFAULT_VALUE = `
-@types:
+_types:
   MyCoolType:
     properties:
-      englishName: $name (expect name parameter)
-      britishName: $name
-      age: $age:25 (default value)
+        names:
+            englishName: $name
+            britishName: $name
+        age: $age:25 # default value
 CoolExample1<MyCoolType>:
   name: SuperYaml
 CoolExample2<MyCoolType>:
   name: Syml
   age: 27
+
 `
 
 function App() {
@@ -27,34 +29,50 @@ function App() {
   const [compiledYamlValue, setCompiledYamlValue] = useState<string>("Loading..")
 
   useEffect(() => {
-    setCompiledYamlValue(compile(yamlValue))
+    try {
+      setCompiledYamlValue(compile(yamlValue))
+    }
+    catch (e) {
+      setCompiledYamlValue(e.message)
+    }
+
   }, [yamlValue])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Super-YAML
+          <p>
+            super-yaml is a tool that helps you write enhanced yaml's and compile them to regular yaml `.yml` files.
+          </p>
         </p>
-        <div style={{display: "flex", justifyContent: "space-evenly", width:"100%"}}>
+        <div style={{display: "flex", justifyContent: "space-evenly", width:"100%", alignItems: "center"}}>
+          <div>
+            Input
           <AceEditor
             mode="yaml"
-            theme="github"
+            theme="chrome"
             onChange={setYamlValue}
             name="yamlInput"
             editorProps={{ $blockScrolling: true }}
             value={yamlValue}
           />
+          </div>
+          <div>
           <ImArrowRight/>
+          </div>
+          <div>
+            Output
           <AceEditor
             readOnly={true}
             mode="yaml"
-            theme="github"
+            theme="chrome"
             onChange={() => {}}
             name="yamlOutput"
             editorProps={{ $blockScrolling: true }}
             value={compiledYamlValue}
           />
+          </div>
         </div>
 
       </header>
